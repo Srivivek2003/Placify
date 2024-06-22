90bd9ab3636d99045870b1ed1d6ce253d3571413
Srivivek2003 <160472925+Srivivek2003@users.noreply.github.com>
2024-06-22 18:53:15 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,11 @@
-# Store this code in 'app.py' file
-
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
-
 
 app = Flask(__name__)
+app.secret_key = 'vivek'
 
-
-app.secret_key = 'your secret key'
-
+# MySQL configurations
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Vivek@123'
@@ -18,57 +13,81 @@ app.config['MYSQL_DB'] = 'vivekdb'
 
 mysql = MySQL(app)
 
-@app.route('/')
-@app.route('/login', methods =['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-		username = request.form['username']
-		password = request.form['password']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM User WHERE username = % s AND password = % s', (username, password, ))
-		account = cursor.fetchone()
-		if account:
-			session['loggedin'] = True
-			session['id'] = account['id']
-			session['username'] = account['username']
-			msg = 'Logged in successfully !'
-			return render_template('index.html', msg = msg)
-		else:
-			msg = 'Incorrect username / password !'
-	return render_template('login.html', msg = msg)
+    msg = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form:
+        name = request.form['name']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE name = %s AND password = %s', (name, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['name'] = account['name']
+            session['password'] = account['password']
+            msg = 'Logged in successfully!'
+            return redirect('/places')
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template('login.html', msg=msg)
 
-@app.route('/logout')
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/places', methods=['GET', 'POST'])
+def places():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM places")
+    places = cur.fetchall()
+    cur.close()
+    return render_template('places.html', places=places)
+
+@app.route('/seema2a', methods=['GET', 'POST'])
+def seema2a():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM places")
+    places = cur.fetchall()
+    cur.close()
+    return render_template('seema2a.html', places=places)
+
+@app.route('/seema1a', methods=['GET', 'POST'])
+def seema1a():
+    return render_template('seema1a.html', places=places)
+
+@app.route('/seema3a', methods=['GET', 'POST'])
+def seema3a():
+    return render_template('seema3a.html', places=places)
+
+@app.route('/seema4a', methods=['GET', 'POST'])
+def seema4a():
+    return render_template('seema4a.html', places=places)
+
+@app.route('/seema5a', methods=['GET', 'POST'])
+def seema5a():
+    return render_template('seema5a.html', places=places)
+
+@app.route('/seema6a', methods=['GET', 'POST'])
+def seema6a():
+    return render_template('seema6a.html', places=places)
+
+@app.route('/seema7a', methods=['GET', 'POST'])
+def seema7a():
+    return render_template('seema7a.html', places=places)
+
+@app.route('/seema8a', methods=['GET', 'POST'])
+def seema8a():
+    return render_template('seema8a.html', places=places)
+
+@app.route('/seema9a', methods=['GET', 'POST'])
+def seema9a():
+    return render_template('seema9a.html', places=places)
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
-	session.pop('loggedin', None)
-	session.pop('id', None)
-	session.pop('username', None)
-	return redirect(url_for('login'))
-
-@app.route('/register', methods =['GET', 'POST'])
-def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM User WHERE username = % s', (username, ))
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'Username must contain only characters and numbers !'
-		elif not username or not password or not email:
-			msg = 'Please fill out the form !'
-		else:
-			cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
-			mysql.connection.commit()
-			msg = 'You have successfully registered !'
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg = msg)
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('name', None) 
+    return redirect('/login')
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
